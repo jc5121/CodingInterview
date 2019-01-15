38384f7e2c9e60f09c92d9fa5e9462c903c6962f
jiaochao <844751258@qq.com>
2019-01-15 08:53:36 +0800

    链表环的入口节点

---
+++ Linked_List/EntryNodeInListLoop.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+
+
+class Solution:
+    def EntryNodeOfLoop(self, pHead):
+        # write code here
+        if pHead is None:
+            return None
+        pFast = pHead
+        pSlow = pHead
+        while pFast.next is not None:
+            pFast = pFast.next.next
+            pSlow = pSlow.next
+            if pFast == pSlow:
+                break
+        if pFast.next is None:
+            return None
+
+        pSlow = pHead
+        while pFast != pSlow:  # 第二次相遇一定就是入口，画图
+            pFast = pFast.next
+            pSlow = pSlow.next
+        return pFast
