4fa2b7ca8ee3cbcd1961ada1949607374c10e3dc
jchao <844751258@qq.com>
2019-09-03 16:05:17 +0800

    Update BSTToList.py

--- Linked_List/BSTToList.py
+++ Linked_List/BSTToList.py
@@ -16,7 +16,7 @@ class Solution:
             return None
         self.arr = list()
         self.midTraversal(pRootOfTree)
-        for i, v in enumerate(self.arr[:-1]):  # 连接数少2
+        for i, v in enumerate(self.arr[:-1]):  # 连接数少2，最后一个节点只添加向前的链，第一个只添加向后的
             v.right = self.arr[i + 1]
             self.arr[i + 1].left = v
         return self.arr[0]
