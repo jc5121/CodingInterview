581e5cd63ccbb0f3e5125138e676f30d77a010ee
jchao <844751258@qq.com>
2019-09-03 16:47:23 +0800

    Update MS_reverse_list.py

--- Linked_List/MS_reverse_list.py
+++ Linked_List/MS_reverse_list.py
@@ -7,7 +7,7 @@ def ReverseList(pHead):
     node = pHead
     if pHead is None or pHead.next is None:  # 1
         return pHead  # 2
-    end_node = ReverseList(node.next)  # 3
+    end_node = ReverseList(node.next)  # 3 先递归了，所以其实是倒着从尾向头调整
     node.next.next = node  # 4 真正调整的在这步
     node.next = None  # 5
     return end_node  # 6
